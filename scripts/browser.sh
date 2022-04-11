d68641c168f7f4f141ea69de597a3cc2b679e8b9
Willi Carlsen <carlsenwilli@gmail.com>
2022-04-11 13:57:08 +0200

    Misc

--- scripts/browser.sh
+++ scripts/browser.sh
@@ -4,7 +4,7 @@ firefox -new-window -url https://gmail.com \
         -new-tab -url https://www.messenger.com \
         -new-tab -url https://outlook.office.com/mail/inbox \
         -new-tab -url https://dfds.slack.com \
-        -new-tab -url https://github.com/dfds/cloudplatform/projects/1 \
+        -new-tab -url https://github.com/orgs/dfds/projects/25 \
         -new-tab -url https://github.com/pulls/review-requested \
         -new-tab -url https://dfds.topdesk.net/tas/secure \
         -new-tab -url https://adfs.dfds.com/adfs/ls/IdpInitiatedSignOn.aspx \
