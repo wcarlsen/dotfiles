b67dead2fcef232d115905b6dbdb5cb4436eadad
Willi Carlsen <carlsenwilli@gmail.com>
2022-11-01 21:25:57 +0100

    Added tabs to neovim

--- neovim/.config/nvim/lua/plugin/init.lua
+++ neovim/.config/nvim/lua/plugin/init.lua
@@ -65,6 +65,9 @@ return require('packer').startup(function(use)
   use { 'hrsh7th/vim-vsnip' }
   use { 'ervandew/supertab' }
 
+  -- Tabs
+  use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'kyazdani42/nvim-web-devicons'}
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
