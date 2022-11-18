1c625205585dbbcb304aa404e5896175805e4484
Willi Carlsen <carlsenwilli@gmail.com>
2022-11-18 15:01:14 +0100

    Added options to neovim

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,36 @@
 local set = vim.opt
 
+-- Spacing and indentation
+set.autoindent = true
+set.smartindent = true
+set.wrap = true
+set.linebreak = true
+set.expandtab = true
+set.tabstop = 2
+set.softtabstop = 2
+set.shiftwidth = 2
+
+-- Files
 set.swapfile = false
+set.backup = false
+set.writebackup = false
+
+-- UI
+set.cursorline = true
+set.showmatch = true
+set.relativenumber = true
+set.termguicolors = true
+
+-- Search
+set.incsearch = true
+set.ignorecase = true
+set.smartcase = true
+
+-- Split
+set.splitbelow = true
+set.splitright = true
+-- Menus set.wildmenu = true
+set.completeopt = "menuone,menu,noselect,noinsert"
+
+-- Encoding
+set.encoding = "utf8"
